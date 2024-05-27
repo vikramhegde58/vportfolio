2706b683c735f44105327bb7cc3126429a7ba958
Vikram Ganapati Hegde <vikramganapatihegde@Vikrams-MacBook-Pro-2.local>
2024-05-27 10:40:56 +0530

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     assetsInlineLimit: 0,
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'exclude-inline-svg',
-          enforce: 'pre',
-          resolveId(source) {
-            if (source.endsWith('.svg')) {
-              return null;
-            }
-          },
-          load(id) {
-            if (id.endsWith('.svg')) {
-              return null;
-            }
-          },
-        },
-      ],
-    },
   },
   server: {
     port: 7777,
   },
-  plugins: [react(), svgr()]
+  plugins: [react()]
 });
